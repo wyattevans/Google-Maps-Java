52dadee57181cbd068cb8a1593f10b67308751b9
Wyatt Evans <syattevans@gmail.com>
2020-07-26 00:05:23 -0600

    Removed logs and updated gradle.

--- app/src/main/java/com/app/googlemapsjava/MapsActivity.java
+++ app/src/main/java/com/app/googlemapsjava/MapsActivity.java
@@ -1,9 +1,8 @@
 package com.app.googlemapsjava;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,13 +12,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    private final String simpleTAG = MapsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        Log.d(simpleTAG, getString(R.string.on_create_hit));
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
@@ -30,7 +27,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d(simpleTAG, getString(R.string.on_map_ready_hit));
         LatLng sydney = new LatLng(-34, 151);
         googleMap.addMarker(new MarkerOptions().position(sydney).title(getString(R.string.marker_in_sydney)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
